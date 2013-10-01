c678724e93fd1e074ee6bb5dc21559acb49ec0f4
Eduardo Mello <emsmello@gmail.com>
2013-10-01 09:28:27 -0300

    second test

--- test.rb
+++ test.rb
@@ -1,8 +1,18 @@
 require 'json'
 require 'json/add/core'
+require 'minigit'
 
 class User < Struct.new(:name, :email); end
 
 admin = User.new()
 admin.name = "Eduardo Mello"
-puts JSON.pretty_generate(admin)
+admin.email = "emsmello@gmai.com"
+json = JSON.pretty_generate(admin)
+puts json
+repo = MiniGit.new()
+repo.checkout 'data'
+
+File.open(admin.class.name + '_' + admin.email, 'w') do |jsonFile|
+        jsonFile.write(json)
+end
+
